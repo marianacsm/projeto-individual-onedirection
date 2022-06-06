12a14018f44fa187990709e2af688c2c296ca8ee
marianacsm <mariana.martins@sptech.school>
2022-06-06 18:17:10 -0300

    Versão final

--- src/database/tabelaspi.sql
+++ src/database/tabelaspi.sql
@@ -1,9 +1,9 @@
 create database projetoindividual;
 use projetoindividual;
 
-create table album (
+create table albumFav (
 idAlbum int primary key auto_increment,
-nomeAlbum varchar(45)
+titulo varchar(45)
 );
 
 create table usuario (
@@ -12,8 +12,19 @@ nomeUsuario varchar(45),
 email varchar(45),
 senha varchar(45),
 fkAlbum int,
-foreign key (fkAlbum) references album (idAlbum)
+foreign key (fkAlbum) references albumFav (idAlbum)
 ) auto_increment=100;
+
+select * from usuario;
+
+DESC usuario;
+insert into albumFav values 
+(null,'Up all night'),
+(null,'Take me home'),
+(null,'Midnight memories'),
+(null,'Four'),
+(null,'Made in the AM');
+
 
 
 
---
+++ src/models/medidaModel.js
@@ -0,0 +1,55 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas(albumFav, limite_linhas) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top ${limite_linhas}
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
+                        momento,
+                        CONVERT(varchar, momento, 108) as momento_grafico
+                    from medida
+                    where fk_aquario = ${idAquario}
+                    order by id desc`;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `SELECT albumFav.titulo, COUNT(fkAlbum) AS quantidade FROM usuario JOIN albumFav ON usuario.fkAlbum = albumFav.idAlbum GROUP BY albumFav.titulo`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(albumFav) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top 1
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
+                        CONVERT(varchar, momento, 108) as momento_grafico, 
+                        fk_aquario 
+                        from medida where fk_aquario = ${idAquario} 
+                    order by id desc`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `SELECT albumFav.titulo, COUNT(fkAlbum) AS quantidade FROM usuario JOIN albumFav ON usuario.fkAlbum = albumFav.idAlbum GROUP BY albumFav.titulo`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
